e8e0bc94ed86848f6de059e51b090784557eac89
kasalava <kasalava86@gmail.com>
2025-05-29 00:01:51 +0500

    end

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const slides = 
+const slides = document.querySelectorAll('.slide'); //объект докбюмент с методом квериселекшен - получаем слайды
+
+for (const slide of slides){    //на каждой итерации создается переменная слайд из массива слайдс
+    slide.addEventListener('click', ()=>{
+        clearActiveClasses()
+        slide.classList.add('active')//при клике - добавляется клаа эктив
+    })
+}
+
+function clearActiveClasses(){ //очищаем активные классы 
+    slides.forEach((slide)=>{
+        slide.classList.remove('active')
+    })
+}
